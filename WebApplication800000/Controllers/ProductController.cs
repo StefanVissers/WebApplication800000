e00c5fbb2c59cf5a12330b219dc5fce007c55bad
TareqSaleem <tareqy2003@hotmail.com>
2017-01-19 14:44:01 +0100

    Update 0.1
    All views, controllers,models & sql connection are added

---
+++ WebApplication800000/Controllers/ProductController.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.UI;
+using WebApplication800000.Models;
+
+namespace WebApplication800000.Controllers
+{
+    public class ProductController : Controller
+    {
+        public static List<ProductModels> products = new List<ProductModels>();
+
+        // GET: Product
+        public ActionResult Singleproduct()
+        {
+            ViewBag.Message = "Your product page.";
+
+            return View();
+        }
+        public ActionResult Browseproduct()
+        {
+            ViewBag.Message = "Your browseproduct page.";
+
+            return View();
+        }
+
+
+        public ActionResult Productshowcase()
+
+        {
+            ViewBag.Message = "Your Productshowcase page.";
+            for (int id = 1; id <= 2; id++)
+
+            {
+                products.Add(new ProductModels(id));
+            }
+
+            return View(products);
+        }
+        public ActionResult Shoppingcart()
+        {
+            ViewBag.Message = "Your Shopping Cart page.";
+            for (int id = 1; id <= 2; id++)
+            {
+                products.Add(new ProductModels(id));
+            }
+            return View();
+        }
+
+    }
+}
---
+++ WebApplication800000/Models/ProductModels.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MySql.Data.MySqlClient;
+using System.Collections;
+using WebApplication800000.Data;
+
+namespace WebApplication800000.Models
+{
+    public class ProductModels
+    {
+        private MySqlConnection conn;
+        private int product_id;
+        private String name;
+        private String catagory;
+        private int price;
+        private String manufactorer;
+        private ArrayList data = new ArrayList();
+
+        public List<int> intArr = new List<int> { 1, 2, 3, 4 };
+        private List<ProductModels> products = new List<ProductModels>();
+        private void BuildProducts(int amount)
+        {
+            for (int i = 0; i < amount; i++)
+            {
+                products.Add(new ProductModels(intArr[i]));
+            }
+        }
+
+        public ProductModels(int _id)
+        {
+            product_id = _id;
+            {
+                conn = Connection.Initialize();
+                conn.Open();
+                MySqlCommand cmd = new MySqlCommand("SELECT * FROM product WHERE product_id=@product_id;", conn);
+                cmd.Prepare();
+                cmd.Parameters.AddWithValue("@product_id", (product_id));
+                using (MySqlDataReader pro = cmd.ExecuteReader())
+                {
+                    while (pro.Read())
+                    {
+                        data.Add(pro[0].ToString());
+                        data.Add(pro[1].ToString());
+                        data.Add(pro[2].ToString());
+                        data.Add(pro[3].ToString());
+                        data.Add(pro[4].ToString());
+
+                    }
+                }
+                FillValues();
+                conn.Close();
+            }
+        }
+        private void FillValues()
+        {
+            name = data[1].ToString();
+            catagory = data[2].ToString();
+            price = Int32.Parse(data[3].ToString());
+            manufactorer = data[4].ToString();
+
+        }
+
+        public String Name
+        {
+            get { return name; }
+        }
+        public String Catagory
+        {
+            get { return catagory; }
+        }
+        public int Price
+        {
+            get { return price; }
+        }
+        public String Manufactorer
+        {
+            get { return manufactorer; }
+        }
+    }
+}
