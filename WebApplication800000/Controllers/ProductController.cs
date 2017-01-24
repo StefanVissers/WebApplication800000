660db56a4bec2c5c814bcb45fef83a75b91f8c3d
TareqSaleem <tareqy2003@hotmail.com>
2017-01-24 10:40:33 +0100

    v0.2 - Cookies & shoppingcart

--- WebApplication800000/Controllers/ProductController.cs
+++ WebApplication800000/Controllers/ProductController.cs
@@ -11,43 +11,91 @@ namespace WebApplication800000.Controllers
     public class ProductController : Controller
     {
         public static List<ProductModels> products = new List<ProductModels>();
+        //public static List<ProductModels> addproducts = new List<ProductModels>();
+        public static List<ProductModels> addedproducts = new List<ProductModels>();
 
-        // GET: Product
+        // GET: Products
         public ActionResult Singleproduct()
         {
             ViewBag.Message = "Your product page.";
-
             return View();
         }
         public ActionResult Browseproduct()
         {
             ViewBag.Message = "Your browseproduct page.";
-
             return View();
         }
+        public ActionResult ikShop(String m)
+        {
+            HttpCookie cookie = new HttpCookie("product");
+
+            cookie.Values.Add(m, m);
 
 
+
+
+            Response.Cookies.Add(cookie);
+            addedproducts.Add(new ProductModels(Int32.Parse(m)));
+            ViewBag.Message = "Your ikShop page.";
+            return RedirectToAction("Shoppingcart", "Product");
+        }
+
+        public ActionResult meShop(String m)
+        {
+            foreach (var x in addedproducts)
+            {
+                if (x.products_id == (Int32.Parse(m)))
+                {
+                    addedproducts.Remove(x);
+                    return RedirectToAction("Shoppingcart", "Product");
+                }
+            }
+            ViewBag.Message = "Your ikShop page.";
+            return RedirectToAction("Shoppingcart", "Product");
+        }
         public ActionResult Productshowcase()
 
+
         {
+            HttpCookie cookie = new HttpCookie("productcookie");
+            cookie.Name = "productcookie";
+            cookie.Values.Add("id", "product_id");
+            cookie.Value = DateTime.Now.ToString();
+            cookie.Expires = DateTime.Now.AddDays(1);
+            Response.Cookies.Add(cookie);
+            var id = Request.Cookies["productcookie"].Value;
+
+            //HttpCookie cookie2 = new HttpCookie("ghjhgf");
+            //cookie2.Values.Add("name", "x");
+            //Response.Cookies.Add(cookie2);
+            //var x = Request.Cookies["productcookie"].Value;
+
+            products.Clear();
             ViewBag.Message = "Your Productshowcase page.";
-            for (int id = 1; id <= 2; id++)
-
+            for (int product_id = 1; product_id <= 100; product_id++)
             {
-                products.Add(new ProductModels(id));
+                products.Add(new ProductModels(product_id));
             }
-
             return View(products);
         }
         public ActionResult Shoppingcart()
         {
-            ViewBag.Message = "Your Shopping Cart page.";
-            for (int id = 1; id <= 2; id++)
+            if (Request.Cookies.Get("product") != null && addedproducts.Count == 0)
             {
-                products.Add(new ProductModels(id));
+                for (int i = 0; i < 1500; i = i + 1)
+                {
+                    if (Request.Cookies.Get("product").Values.Get(i + "") != null)
+                    {
+                        addedproducts.Add(new ProductModels(i));
+                    }
+                }
             }
-            return View();
-        }
 
+
+            Request.Cookies.Get("productcookie");
+            ViewBag.Message = "Your Shopping Cart page.";
+            
+            return View(addedproducts);
+        }
     }
 }
