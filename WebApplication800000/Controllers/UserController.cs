cbe5c861c743d6424e2bc28f0924ac465e01f4ea
TareqSaleem <tareqy2003@hotmail.com>
2017-01-31 17:26:49 +0100

    Update 0.5

---
+++ WebApplication800000/Controllers/UserController.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication800000.Models;
+using WebApplication800000.Data;
+using MySql.Data.MySqlClient;
+
+namespace WebApplication800000.Controllers
+{
+    public class UserController : Controller
+    {
+
+        public static List<UserModels> userlist = new List<UserModels>();
+        public ActionResult Edit()
+        {
+            HttpCookie cookieuser = new HttpCookie("usercookie");
+            cookieuser.Name = "usercookie";
+            cookieuser.Values.Add("id", "name");
+            cookieuser.Value = DateTime.Now.ToString();
+            cookieuser.Expires = DateTime.Now.AddDays(1);
+            Response.Cookies.Add(cookieuser);
+            var id = Request.Cookies["usercookie"].Value;
+            ViewBag.Message = "Your Edit page.";
+            return View(userlist);
+        }
+
+        // GET: User
+        public ActionResult addUser(String m)
+        {
+            HttpCookie usercookie = new HttpCookie("user");
+            usercookie.Values.Add(m, m);
+            Response.Cookies.Add(usercookie);
+            userlist.Add(new UserModels(Int32.Parse(m)));
+            ViewBag.Message = "Your adduser page.";
+            return RedirectToAction("Userwishlist", "Product");
+        }
+        public ActionResult remUser(String m)
+        {
+            foreach (var x in userlist)
+            {
+                if (x.Id == (Int32.Parse(m)))
+                {
+                    userlist.Remove(x);
+                    return RedirectToAction("Userwishlist", "Product");
+                }
+            }
+            ViewBag.Message = "Your remUser page.";
+            return RedirectToAction("Userwishlist", "Product");
+        }
+
+        public ActionResult Userwishlist()
+        {
+            userlist.Clear();
+            ViewBag.Message = "Your Productshowcase page.";
+            if (Request.Cookies.Get("usercookie") != null && userlist.Count == 0)
+            {
+                for (int x = 0; x < 1500; x = x + 1)
+                {
+                    if (Request.Cookies.Get("usercookie").Values.Get(x + "") != null)
+                    {
+                        userlist.Add(new UserModels(x));
+                    }
+                }
+            }
+            Request.Cookies.Get("usercookie");
+            ViewBag.Message = "Your Userwishlist page.";
+            return View(userlist);
+        }
+    }
+}
---
+++ WebApplication800000/Models/UserModels.cs
@@ -0,0 +1,70 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using WebApplication800000.Data;
+
+namespace WebApplication800000.Models
+{
+    public class UserModels
+    {
+        private MySqlConnection conn;
+        private int id;
+        private String name;
+        private ArrayList data = new ArrayList();
+
+        public List<int> intaddArruser = new List<int> { 1, 2 };
+        private List<UserModels> userlist = new List<UserModels>();
+
+
+
+        public void BuildUser(int amount)
+        {
+            for (int x = 0; x < amount; x++)
+            {
+                userlist.Add(new UserModels(intaddArruser[x]));
+            }
+        }
+
+        public UserModels(int _id)
+        {
+            id = _id;
+            {
+                conn = Connection.Initialize();
+                conn.Open();
+                MySqlCommand cmd = new MySqlCommand("SELECT * FROM customer_id WHERE id=@id;", conn);
+                cmd.Prepare();
+                cmd.Parameters.AddWithValue("@id", (id));
+                using (MySqlDataReader pro = cmd.ExecuteReader())
+                {
+                    while (pro.Read())
+                    {
+                        data.Add(pro[0].ToString());
+                        data.Add(pro[1].ToString());
+
+                    }
+                }
+                FillValues();
+                conn.Close();
+            }
+        }
+        private void FillValues()
+        {
+            id = Int32.Parse(data[0].ToString());
+            name = data[1].ToString();
+
+        }
+        public int Id
+        {
+            get { return id; }
+        }
+        public String Name
+        {
+            get { return name; }
+        }
+    }
+
+
+}
